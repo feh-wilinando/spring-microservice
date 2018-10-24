49cc15d87ad0ad1b3a8a4c6f9707c0ab24617df9
Fernando <feh.wilinando@gmail.com>
2018-10-23 22:50:03 -0300

    Added hystrix service

---
+++ hystrix-dashboard/src/main/java/microservice/hystrix/HystrixDashboardApp.java
@@ -0,0 +1,13 @@
+package microservice.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApp.class, args);
+    }
+}
